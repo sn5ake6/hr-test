e39efaf8797516ee1237b5dd547c57b3ad9ff2a6
sn5ake6 <sn5ake6@gmail.com>
2017-06-17 10:02:49 +0500

    first commit

---
+++ php/brackets.php
@@ -0,0 +1,25 @@
+<?php
+function process_bracket($stack, $bracket) {
+    $bracket_map = ['{' => '}', '[' => ']', '(' => ')'];
+    if (array_key_exists($bracket, $bracket_map)) {
+      $stack[] = $bracket;
+    } else {
+      $open_bracket = array_search($bracket, $bracket_map);
+      $last_bracket = array_pop($stack);
+      if ($last_bracket != $open_bracket) {
+          $stack[] = $last_bracket;
+          $stack[] = $bracket;
+      };
+    };
+    return $stack;
+};
+
+function parse($item) {
+    $stack = array_reduce(str_split($item), 'process_bracket', []);
+    return count($stack) == 0 ? 'YES' : 'NO';
+};
+
+function braces($values) {
+    return array_map('parse', $values);
+};
+?>
---
+++ php/sum2numbers.php
@@ -0,0 +1,23 @@
+<?php
+$handle = fopen ("php://stdin", "r");
+$input = fgets($handle);
+list($number_1, $number_2) = explode(" ", $input);
+$number_1 = str_split($number_1);
+$number_2 = str_split($number_2);
+$add_one = 0;
+$result = [];
+while (count($number_1) + count($number_2) > 0){
+    $digit_1 = array_pop($number_1) ?? 0;
+    $digit_2 = array_pop($number_2) ?? 0;
+    $summa = $digit_1 + $digit_2 + $add_one;
+    $add_one = 0;
+    if ($summa >= 10) {
+        $summa -= 10;
+        $add_one = 1;
+    }
+    $result[] = $summa;
+}
+if ($add_one == 1) $result[] = $add_one;
+$result = implode(array_reverse($result));
+echo $result;
+?>
